76aa7c191b7eb63d5bc4f97a17146ff5f705abf7
bbdk612 <smsmisha51@gmail.com>
2023-04-23 14:38:52 +0500

    Created a constructor for Matrix

--- algorithms/A/Matrix.js
+++ algorithms/A/Matrix.js
@@ -1,12 +1,27 @@
 class Matrix {
 	constructor(size) {
 		this.matrix = Array(size).fill(Array(size));
+		for (let i = 0; i < size; i++) {
+			for (let j = 0; j < size; j++) {
+				if (j != i) {
+					this.matrix[i][j] = 1;
+				} else {
+					this.matrix[i][j] = 0;
+				}
+			}
+		}
+		let numberOfNoWayNodes = Math.floor(Math.random() * (size - 1 + 1)) + 1;
 		this.noWayNodes = Array();
-		
-		let noWayNode = Math.floor(Math.random() * (size - 1 + 1)) + 1
+		let noWayNode;
+		do {
+			noWayNode = Math.floor(Math.random() * (size - 1 + 1)) + 1;
+			if (!(noWayNode in this.noWayNodes)) {
+				this.noWayNodes.push(noWayNode)
+			}
+		} while(noWayNode.length !== numberOfNoWayNodes);
 	}
 	
-	generateNoWayNodes() {
+	regenerateNoWayNodes() {
 		
 	}
 }
