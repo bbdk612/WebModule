a4f020456bc9bf5a212755d209d3aee6878baaa3
bbdk612 <smsmisha51@gmail.com>
2023-04-23 22:41:35 +0500

    Created class matrix for graph

--- algorithms/A/Matrix.js
+++ algorithms/A/Matrix.js
@@ -1,16 +1,6 @@
 class Matrix {
 	constructor(size) {
-		this.matrix = Array(size).fill(Array(size));
-		for (let i = 0; i < size; i++) {
-			for (let j = 0; j < size; j++) {
-				if (j != i) {
-					this.matrix[i][j] = 1;
-				} else {
-					this.matrix[i][j] = 0;
-				}
-			}
-		}
-		let numberOfNoWayNodes = Math.floor(Math.random() * (size - 1 + 1)) + 1;
+		let numberOfNoWayNodes = Math.floor(Math.random() * ((size - 2) - 1 + 1)) + 1;
 		this.noWayNodes = Array();
 		let noWayNode;
 		do {
@@ -19,6 +9,23 @@ class Matrix {
 				this.noWayNodes.push(noWayNode)
 			}
 		} while(noWayNode.length !== numberOfNoWayNodes);
+
+		this.start = -1;
+		this.finish = -1;
+		while (this.start === -1) {
+			this.start = Math.floor(Math.random() * (size - 1 + 1)) + 1;
+			if (this.start in noWayNode) {
+				this.start = -1;
+			}
+		}
+		while (this.finish === -1) {
+			this.finish = Math.floor(Math.random() * (size - 1 + 1)) + 1;
+			if ((this.finish in noWayNode) || (this.finish === this.start)) {
+				this.finish = -1;
+			}
+		}
+
+		this.matrix = Array(size).fill(Array(size));
 	}
 	
 	regenerateNoWayNodes() {
