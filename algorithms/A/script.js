58345b7eb2dde99b6f6cb6d873272ddc1a992954
bbdk612 <smsmisha51@gmail.com>
2023-04-27 15:19:36 +0500

    created generation and buttons for edit

--- algorithms/A/script.js
+++ algorithms/A/script.js
@@ -1,18 +1,36 @@
-let matrixSize;
+let editButtons = document.querySelectorAll(".edit__button:not(#stop-no-way-node)");
+
+for (let editButton of editButtons) {
+	editButton.disabled = true;
+}
+
+let graphSizeInput = document.querySelector(".size__input");
 let buttonClicked = false;
 let graph;
 const button = document.querySelector(".size__button");
+
+let matrixSize;
+
+graphSizeInput.addEventListener("change", () => {
+	if (parseInt(graphSizeInput.value) > 100) {
+		button.disabled = true;
+	} else {
+		button.disabled = false;
+	}
+});
+
 button.addEventListener("click", () => {
 	matrixSize = parseInt(document.querySelector(".size__input").value);
 	buttonClicked = true;
-	console.log(matrixSize);
+//	console.log(matrixSize);
 	graph = new Graph(matrixSize);
 
-	let graphLine = '';
+	let graphLine = '<h2>Граф</h2>';
 	for (let i = 0; i < matrixSize; i++) {
+
 		graphLine += `<div class="graph-line">`
 		for (let j = 0; j < matrixSize; j++) {
-			graphLine += `<div class="graph-node" id="${graph.M[i][j]}">${graph.M[i][j]}</div>`
+			graphLine += `<div class="graph-node" id="${graph.M[i][j]}"></div>`
 		}
 		graphLine += "</div>"
 	}
@@ -21,7 +39,7 @@ button.addEventListener("click", () => {
 	G.innerHTML += graphLine;
 	graph.noWayNodes.forEach((nodeNumber) => {
 		let node = document.querySelector(`[id="${nodeNumber}"]`);
-		node.classList.add( "no-way-node");
+		setTimeout(()=>{node.classList.add( "no-way-node");}, 500);
 	})
 
 	let start = document.querySelector(`[id="${graph.start}"]`);
@@ -29,5 +47,13 @@ button.addEventListener("click", () => {
 
 	let finish = document.querySelector(`[id="${graph.finish}"]`);
 	finish.classList.add("finish");
+
+	for (let editButton of editButtons) {
+		editButton.disabled = false;
+	}
 });
 
+let startEditNoWay = document.querySelector("#start-no-way-node");
+startEditNoWay.addEventListener("click", () => {
+	
+});
